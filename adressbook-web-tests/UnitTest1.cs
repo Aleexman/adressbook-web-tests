e4d2815566b5f83f28ff26ed9e4ff2ab3331692a
askuzenkov <askuzenkov@0000NBB01SD2533>
2022-06-14 16:37:52 +0300

    Add project files.

---
+++ adressbook-web-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace adressbook_web_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
